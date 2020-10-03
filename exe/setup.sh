f40855334e908fd34b08a0e6070a7cd8b86dbf77
Alexander Popov <alex.wayfer@gmail.com>
2020-10-04 00:06:26 +0300

    Add `git pull` commands for `update` script

--- exe/setup.sh
+++ exe/setup.sh
@@ -4,9 +4,6 @@ CURRENT_DIR=`dirname "$0"`
 
 . $CURRENT_DIR/_common.sh
 
-# exe git checkout $1
-# exe git pull origin $1
-
 exe $CURRENT_DIR/setup/ruby.sh
 
 exe toys config check
--- exe/update.sh
+++ exe/update.sh
@@ -6,6 +6,9 @@ CURRENT_DIR=`dirname "$0"`
 
 exe systemctl --user stop stream_timer
 
+exe git checkout $1
+exe git pull origin $1
+
 exe $CURRENT_DIR/setup.sh "$@"
 
 exe toys db migrate
