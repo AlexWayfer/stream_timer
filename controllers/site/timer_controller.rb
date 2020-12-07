d8ba512f050aeb0bb70b48dc7011cd026e4d1db1
Alexander Popov <alex.wayfer@gmail.com>
2020-12-07 22:33:42 +0300

    Add timer preview to configuration page

--- controllers/site/timer_controller.rb
+++ controllers/site/timer_controller.rb
@@ -4,14 +4,25 @@ module StreamTimer
 	module Site
 		## Controller for timer page
 		class TimerController < Site::Controller
-			def index(key)
-				find_form_outcome = Forms::Configuration::Find.new(key: key).run
+			include ST::ConfigurationsHelper
 
-				if find_form_outcome.success? && (configuration = find_form_outcome.result)
-					view :index, scope: transform_to_view_object(configuration)
-				else
-					halt 404
-				end
+			def index(key)
+				configuration = find_configuration key, check_user: false
+
+				view :index, scope: transform_to_view_object(configuration)
+			end
+
+			def demo
+				configuration = initialize_configuration_create_form.instance
+
+				view :index, scope: transform_to_view_object(configuration)
+			end
+
+			private
+
+			## For `configuration_params`
+			def current_user
+				nil
 			end
 		end
 	end
