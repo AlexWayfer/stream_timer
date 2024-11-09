98b04476e8d0a5c29c3fa3be3d769ec1e940336a
Alexander Popov <alex.wayfer@gmail.com>
2024-11-09 17:19:10 +0300

    Update Twitch configuration for new Twitch API gem
    It has to be done a long time ago.

--- config/processors/twitch.rb
+++ config/processors/twitch.rb
@@ -8,9 +8,15 @@ module StreamTimer
 			## Initialize Twitch API client
 			class Twitch
 				def initialize(config)
+					config[:twitch][:tokens] = ::TwitchOAuth2::Tokens.new(
+						client: {
+							client_id: config[:twitch][:client_id],
+							client_secret: config[:twitch][:client_secret]
+						}
+					)
+
 					config[:twitch][:client] = ::Twitch::Client.new(
-						client_id: config[:twitch][:client_id],
-						client_secret: config[:twitch][:client_secret]
+						tokens: config[:twitch][:tokens]
 					)
 				end
 			end
