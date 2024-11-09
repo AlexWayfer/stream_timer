c69459472cfe08ff7588b3ef73743df8d6ef3040
Alexander Popov <alex.wayfer@gmail.com>
2024-11-09 17:16:33 +0300

    Resolve new RuboCop offenses about DB inversible migrations
    I hope everything is OK.

--- db/migrations/20221011145039_add_timestamps_to_providers_users.rb
+++ db/migrations/20221011145039_add_timestamps_to_providers_users.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
 Sequel.migration do
-	change do
-		%i[twitch_users google_users].each do |table_name|
+	users_tables = %i[twitch_users google_users].freeze
+
+	up do
+		users_tables.each do |table_name|
 			alter_table table_name do
 				add_column :created_at, :timestamptz, null: false, default: Sequel::CURRENT_TIMESTAMP
 				add_column :updated_at, :timestamptz
+			end
+		end
+	end
+
+	down do
+		users_tables.each do |table_name|
+			alter_table table_name do
+				drop_column :created_at
+				drop_column :updated_at
 			end
 		end
 	end
